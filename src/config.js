d1437eab73961fed07b6660ff8642aca4c97a60a
Innocent98 <tosinadebayo55@gmail.com>
2022-04-16 00:03:47 +0100

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://locate-auto-engineer.herokuapp.com/api/",
+  baseURL: "https://forezon-api.herokuapp.com/api/",
 });
